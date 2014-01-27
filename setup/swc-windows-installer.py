3f7406ee909ccef1d0299fc5c3fc1ecb9e21834c
unknown <Neal Davis>
2014-01-27 14:32:46 -0600

    Some people have spaces in their Windows usernames, but this script didn't have quotes around the /c/Users/Neal Davis/bin:.:/usr/local/bin:/mingw/bin:/bin:/c/Windows/system32:/c/Windows:/c/Windows/System32/Wbem:/c/Windows/System32/WindowsPowerShell/v1.0/:/c/Program Files/MATLAB/R2013b/runtime/win64:/c/Program Files/MATLAB/R2013b/bin:/c/Users/Neal Davis/AppData/Local/Enthought/Canopy/User:/c/Users/Neal Davis/AppData/Local/Enthought/Canopy/User/Scripts:/c/Users/Neal:/c/Users/Neal variable, leading to trouble.

--- setup/swc-windows-installer.py
+++ setup/swc-windows-installer.py
@@ -83,9 +83,9 @@ def update_bash_profile(extra_paths=()):
     lines = [
         '',
         '# Add paths for Software-Carpentry-installed scripts and executables',
-        'export PATH=$PATH:{}'.format(':'.join(
+        'export PATH=\"$PATH:{}\"'.format(':'.join(
             make_posix_path(path) for path in extra_paths),),
-        '',
+	'',
         '# Make nano the default editor',
         'export EDITOR=nano',
         '',
